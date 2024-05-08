87cb22d6e3fab31444f1ba67ea3ff7876345a0f3
George-Anyas <georgelove462@gmail.com>
2024-05-09 01:51:04 +0300

    add floating action butoom

---
+++ lib/view/widgets/notes_view_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:notes_app/view/widgets/custom_appbar.dart';
+
+import 'package:notes_app/view/widgets/notes_listView.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
+        ],
+      ),
+    );
+  }
+}
