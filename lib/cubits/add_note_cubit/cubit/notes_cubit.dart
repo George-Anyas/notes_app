fd9b2b82d6f7e6cea05c3193a4f8d1f1fd45bcae
George-Anyas <georgelove462@gmail.com>
2024-05-23 14:16:42 +0300

    display notes part one

--- lib/cubits/add_note_cubit/cubit/notes_cubit.dart
+++ lib/cubits/add_note_cubit/cubit/notes_cubit.dart
@@ -9,16 +9,9 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
-  fetchAllNotes() async {
-    emit(NotesLoading());
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      Notesfailure(e.toString());
-    }
+  List<NoteModel>? notes;
+  fetchAllNotes() {
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    List<NoteModel> notes = notesBox.values.toList();
   }
 }
